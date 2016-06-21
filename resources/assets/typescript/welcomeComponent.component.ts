a95b2995a0d29d70dbe81332cb0e9074dbe3287b
paulsthorpe <paulsthorpe@gmail.com>
2016-06-20 21:26:27 -0400

    cart price fix

--- resources/assets/typescript/welcomeComponent.component.ts
+++ resources/assets/typescript/welcomeComponent.component.ts
@@ -49,7 +49,7 @@ export class WelcomeComponent implements OnInit{
 
   retrieveOrderState(orderIndex){
     this.reorderService.retrieveOrderState(orderIndex);
-    this.router.navigate(['Review']);
+    // this.router.navigate(['Review']);
   }
 
   applyOrder(data, orderIndex, itemIndex){
