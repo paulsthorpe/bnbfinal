7824278d0483fc7a7c138957160492f92a3d0264
paulsthorpe <paulsthorpe@gmail.com>
2016-06-20 21:14:38 -0400

    cart price fix

--- public/js/cart.component.js
+++ public/js/cart.component.js
@@ -33,14 +33,10 @@ System.register(['@angular/core', '@angular/http', '@angular/router-deprecated',
                 }
                 CartComponent.prototype.ngOnInit = function () {
                     this.cart = this.orderService.cart;
-                    console.log(1);
-                    if (this.cart.length > 0) {
-                        console.log(1);
-                        this.totalPrice = this.orderService.itemPrices.reduce(function (total, num) { return total + num; });
-                        console.log(this.totalPrice);
-                        this.tax = this.orderService.calcTax(this.totalPrice);
-                        this.subtotal = this.tax + this.totalPrice;
-                    }
+                    this.totalPrice = this.orderService.itemPrices.reduce(function (total, num) { return total + num; });
+                    console.log(this.totalPrice);
+                    this.tax = this.orderService.calcTax(this.totalPrice);
+                    this.subtotal = this.tax + this.totalPrice;
                 };
                 CartComponent.prototype.deleteItem = function (item) {
                     //delete objects related to user input
