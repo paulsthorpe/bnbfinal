ca076e3b6c40278c0cb4040887b74d4a080ad65d
paulsthorpe <paulsthorpe@gmail.com>
2016-06-28 11:56:08 -0400

    load screen done

--- resources/views/order_app.blade.php
+++ resources/views/order_app.blade.php
@@ -31,7 +31,16 @@
         <div class="loader">
           <i class="fa fa-spinner fa-pulse"></i>
           <p class="loading">
-            Loading...
+            <span>L</span>
+            <span>o</span>
+            <span>a</span>
+            <span>d</span>
+            <span>i</span>
+            <span>n</span>
+            <span>g</span>
+            <span>.</span>
+            <span>.</span>
+            <span>.</span>
           </p>
         </div>
 
@@ -58,11 +67,58 @@
         font-size: 2em;
         color: white;
       }
+      .loading span {
+        opacity: 0;
+        transition: all .05s;
+      }
+      .loading span:nth-child(1) {
+        transition-delay: 0.08s;
+      }
+      .loading span:nth-child(2) {
+        transition-delay: 0.16s;
+      }
+      .loading span:nth-child(3) {
+        transition-delay: 0.24s;
+      }
+      .loading span:nth-child(4) {
+        transition-delay: 0.32s;
+      }
+      .loading span:nth-child(5) {
+        transition-delay: 0.4s;
+      }
+      .loading span:nth-child(6) {
+        transition-delay: 0.48s;
+      }
+      .loading span:nth-child(7) {
+        transition-delay: 0.56s;
+      }
+      .loading span:nth-child(8) {
+        transition-delay: 0.64s;
+      }
+      .loading span:nth-child(9) {
+        transition-delay: 0.72s;
+      }
+      .loading span:nth-child(10) {
+        transition-delay: 0.80s;
+      }
+      .loading span:nth-child(11) {
+        transition-delay: .88s;
+      }
+      .loading.active span {
+        opacity: 1;
+      }
+
+
 
 
     </style>
 </html>
 <script>
+
+$(document).ready(function(){
+  $('.loading').addClass('active');
+});
+
 System.config({
     "defaultJSExtensions": true,
     packages: {
